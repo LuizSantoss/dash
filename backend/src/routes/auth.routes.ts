12d23c522065937c400b22a0b80bbbdc163a231a
LuizSantoss <lzfelipepro@outlook.com>
2026-07-29 09:52:00 -0300

    feat: updating routes

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import { registrar, login } from '../controllers/auth.controller.ts'
+import { registrar, login, listarUsuarios, alterarSenhaUsuario } from '../controllers/auth.controller.ts';
+import { verificarToken } from "../middleware/auth.middleware.ts";
 
 const router = Router();
 
+// Rotas públicas (Abertas)
 router.post('/registrar', registrar);
 router.post('/login', login);
+
+// Rotas protegidas (Exclusivas do ADM - a barreira de perfil está no controller)
+router.get('/usuarios', verificarToken, listarUsuarios);
+router.put('/usuarios/alterar-senha', verificarToken, alterarSenhaUsuario);
 
 export default router;
--- backend/src/routes/requisicao.routes.ts
+++ backend/src/routes/requisicao.routes.ts
@@ -6,7 +6,8 @@ import {
     encaminharDiretoria,
     listarRequisicoesDiretoria,
     avaliarRequisicao,
-    listarHistoricoDiretoria
+    listarHistoricoDiretoria,
+    listarTodasRequisicoesADM // <-- Função importada do ADM
 } from "../controllers/requisicao.controller.ts";
 import { verificarToken } from "../middleware/auth.middleware.ts";
 
@@ -15,14 +16,13 @@ const router = Router();
 // Rotas de consulta (GET)
 router.get('/minhas', verificarToken, listarMinhasRequisicoes);
 router.get('/rh', verificarToken, listarRequisicoesRH);
-router.get('/diretoria', verificarToken, listarRequisicoesDiretoria); // <-- Nova rota
-router.get('/diretoria/historico', verificarToken, listarHistoricoDiretoria)
+router.get('/diretoria', verificarToken, listarRequisicoesDiretoria); 
+router.get('/diretoria/historico', verificarToken, listarHistoricoDiretoria);
+router.get('/adm/todas', verificarToken, listarTodasRequisicoesADM); // <-- Nova rota do ADM
 
 // Rotas de criação e atualização (POST / PUT)
 router.post('/', verificarToken, criarRequisicao);
 router.put('/:id/encaminhar-diretoria', verificarToken, encaminharDiretoria);
-router.put('/:id/avaliar', verificarToken, avaliarRequisicao); // <-- Nova rota
-
-
+router.put('/:id/avaliar', verificarToken, avaliarRequisicao); 
 
 export default router;
